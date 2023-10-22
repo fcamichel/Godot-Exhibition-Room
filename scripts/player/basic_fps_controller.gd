901b72fdb8db61297ac61a705ccbc45f3b7793fc
Fabio Armon Camichel <fabiocamichel@gmail.com>
2023-10-22 10:25:29 +0200

    feat: starting with logic of exhibition room

--- scripts/player/basic_fps_controller.gd
+++ scripts/player/basic_fps_controller.gd
@@ -1,5 +1,11 @@
 extends CharacterBody3D
 
+# initialize required variables
+@onready var user_choices = []
+
+# TODO: initialize hotspots on specific nodes (models)
+
+
 func _ready():
 	
 	pass
@@ -7,8 +13,17 @@ func _ready():
 
 func _input(event):
 	
-	# click
+	# check for click event
 	
 	if event.is_action_pressed("click"):
-		print("click event triggered")
-
+		user_choices.append("click")
+		print(user_choices)
+		
+		# TODO: check if on hotspot / correct node
+		# TODO: logic of node manipulations (show other page of book
+		#       or other clothing on model).Using the user_choices array
+		#       to store information about where the user clicked.
+		
+		pass
+		
+	pass
